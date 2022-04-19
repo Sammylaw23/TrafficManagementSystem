47be414f28c4e8cca6ef2599715c71808b7b696f
Samuel Lawal <salawal@axxess.com>
2022-04-19 14:06:55 -0500

    Repository Layer Added with UnitOfWork

---
+++ traffic-management-system.Application/Interfaces/IRepositoryProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace traffic_management_system.Application.Interfaces
+{
+    public interface IRepositoryProvider
+    {
+        public IDriverRepository DriverRepository { get; }
+        Task SaveChangesAsync();
+    }
+}
