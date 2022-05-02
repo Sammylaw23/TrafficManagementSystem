f045cbdf8d739160e9b57a52fb2178dcb5509302
sodiqyekeen <sodiq.yekeen.o@gmail.com>
2022-05-02 17:16:27 +0300

    Improve driver dialog ui

---
+++ src/TrafficManagementSystem.UI.Infrastructure/Constants/AppConstants.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor;
+
+namespace TrafficManagementSystem.UI.Infrastructure.Constants
+{
+    public static class AppConstants
+    {
+        public static class Designs
+        {
+            public const Variant InputTextVariant = Variant.Text;
+            public const Variant ButtonVariant = Variant.Filled;
+        }
+    }
+}
